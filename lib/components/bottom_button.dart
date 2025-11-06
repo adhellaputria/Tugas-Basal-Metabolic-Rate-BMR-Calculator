ad828580cd4e402b9d574750916a23d3367c93d2
adhellaputria <adhellaputria912@gmail.com>
2025-11-06 16:05:38 +0700

    bottom_button.dart

--- lib/components/bottom_button.dart
+++ lib/components/bottom_button.dart
@@ -1,21 +1,24 @@
-import 'package:flutter/material.dart';
-import 'package:bmi_app/constants.dart';
+import 'package:flutter/material.dart';  // Mengimpor paket Material Design untuk membangun antarmuka pengguna
+import 'package:bmi_app/constants.dart';  // Mengimpor konstanta untuk warna dan gaya teks
 
+// Widget BottomButton untuk menampilkan tombol di bagian bawah layar
 class BottomButton extends StatelessWidget {
+  // Konstruktor untuk menerima judul tombol dan aksi saat tombol ditekan
   BottomButton({required this.buttonTitle, required this.onTap});
 
-  final String buttonTitle;
-  final Function() onTap;
+  final String buttonTitle;  // Judul teks yang ditampilkan pada tombol
+  final Function() onTap;    // Fungsi yang dijalankan saat tombol ditekan
 
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
-      onTap: onTap,
+      // Menangani interaksi sentuhan dengan tombol
+      onTap: onTap,  // Menjalankan aksi yang diteruskan ke parameter onTap
       child: Container(
-        margin: const EdgeInsets.only(top: 10.0),
-        height: bottomContainerHeight,
-        color: bottomContainerColor,
-        child: Center(child: Text(buttonTitle, style: buttonTextStyle)),
+        margin: const EdgeInsets.only(top: 10.0),  // Memberikan jarak atas pada tombol
+        height: bottomContainerHeight,  // Menentukan tinggi tombol
+        color: bottomContainerColor,  // Menetapkan warna latar belakang tombol
+        child: Center(child: Text(buttonTitle, style: buttonTextStyle)),  // Menampilkan teks tombol di tengah
       ),
     );
   }
