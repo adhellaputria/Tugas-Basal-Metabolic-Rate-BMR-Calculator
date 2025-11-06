7f3f22c46d7f6f548c31ef99565ec36acf2e0f84
adhellaputria <adhellaputria912@gmail.com>
2025-11-06 16:09:21 +0700

    round_icon_button.dart

--- lib/components/round_icon_button.dart
+++ lib/components/round_icon_button.dart
@@ -1,20 +1,22 @@
-import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';  // Mengimpor paket Material Design untuk membangun antarmuka pengguna
 
+// Widget RoundIconButton untuk menampilkan tombol berbentuk bulat dengan ikon di dalamnya
 class RoundIconButton extends StatelessWidget {
+  // Konstruktor menerima ikon dan fungsi yang dijalankan saat tombol ditekan
   RoundIconButton({required this.icon, required this.onPressed});
 
-  final IconData icon;
-  final Function() onPressed;
+  final IconData icon;  // Ikon yang ditampilkan di dalam tombol
+  final Function() onPressed;  // Fungsi yang dijalankan saat tombol ditekan
 
   @override
   Widget build(BuildContext context) {
     return RawMaterialButton(
-      elevation: 0.0,
-      constraints: const BoxConstraints.tightFor(width: 40.0, height: 40.0),
-      shape: const CircleBorder(),
-      fillColor: const Color(0xFF4C4F5E),
-      onPressed: onPressed,
-      child: Icon(icon),
+      elevation: 0.0,  // Menghapus efek bayangan pada tombol
+      constraints: const BoxConstraints.tightFor(width: 40.0, height: 40.0),  // Menetapkan ukuran tombol tetap
+      shape: const CircleBorder(),  // Membuat bentuk tombol menjadi lingkaran
+      fillColor: const Color(0xFF4C4F5E),  // Menentukan warna latar tombol
+      onPressed: onPressed,  // Menetapkan aksi yang dijalankan saat tombol ditekan
+      child: Icon(icon),  // Menampilkan ikon di dalam tombol
     );
   }
 }
