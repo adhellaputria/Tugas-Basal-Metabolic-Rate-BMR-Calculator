f7f44fd2d7544b62bb4b11020bab575a8058053f
adhellaputria <adhellaputria912@gmail.com>
2025-11-06 16:06:28 +0700

    Update icon_card.dart

--- lib/components/icon_card.dart
+++ lib/components/icon_card.dart
@@ -1,19 +1,23 @@
-import 'package:flutter/material.dart';
-import 'package:bmi_app/constants.dart';
+import 'package:flutter/material.dart';  // Mengimpor paket Material Design untuk membangun antarmuka pengguna
+import 'package:bmi_app/constants.dart';  // Mengimpor konstanta untuk gaya teks dan elemen lainnya
 
+// Widget IconCard untuk menampilkan kartu dengan ikon dan teks
 class IconCard extends StatelessWidget {
+  // Konstruktor untuk menerima ikon dan caption (teks)
   IconCard({this.cardIcon, required this.caption});
 
-  final IconData? cardIcon;
-  final String caption;
+  final IconData? cardIcon;  // Ikon yang akan ditampilkan dalam kartu
+  final String caption;     // Teks yang akan ditampilkan di bawah ikon
 
   @override
   Widget build(BuildContext context) {
     return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
+      mainAxisAlignment: MainAxisAlignment.center,  // Menyusun elemen di tengah kolom
       children: [
+        // Menampilkan ikon dengan ukuran 70
         Icon(cardIcon, size: 70.0),
-        const SizedBox(height: 15.0),
+        const SizedBox(height: 15.0),  // Memberikan jarak vertikal antara ikon dan teks
+        // Menampilkan caption (teks) dengan gaya labelTextStyle
         Text(caption, style: labelTextStyle),
       ],
     );
